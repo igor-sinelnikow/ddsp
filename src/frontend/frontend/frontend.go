5ecdc694959d644d1852b783326d7fdbe3e3def7
Igor <igor.sinelnikow+git@gmail.com>
2018-12-20 01:33:09 +0300

    New, Put, Del

--- src/frontend/frontend/frontend.go
+++ src/frontend/frontend/frontend.go
@@ -38,15 +38,45 @@ type Config struct {
 
 // Frontend is a frontend service.
 type Frontend struct {
-	// TODO: implement
+	cfg Config
 }
 
 // New creates a new Frontend with a given cfg.
 //
 // New создает новый Frontend с данным cfg.
 func New(cfg Config) *Frontend {
-	// TODO: implement
-	return nil
+	return &Frontend{cfg: cfg}
+}
+
+func (fe *Frontend) handleErrors(k storage.RecordID,
+	action func(node storage.ServiceAddr) error) error {
+	nodes, err := fe.cfg.RC.NodesFind(fe.cfg.Router, k)
+	if err != nil {
+		return err
+	}
+	if len(nodes) < storage.MinRedundancy {
+		return storage.ErrNotEnoughDaemons
+	}
+
+	errors := make(chan error, len(nodes))
+	for _, node := range nodes {
+		go func(node storage.ServiceAddr) {
+			errors <- action(node)
+		}(node)
+	}
+
+	count := make(map[error]int)
+	for range nodes {
+		count[<-errors]++
+	}
+
+	for err, num := range count {
+		if num >= storage.MinRedundancy {
+			return err
+		}
+	}
+
+	return storage.ErrQuorumNotReached
 }
 
 // Put an item to the storage if an item for the given key doesn't exist.
@@ -55,8 +85,9 @@ func New(cfg Config) *Frontend {
 // Put -- добавить запись в хранилище, если запись для данного ключа
 // не существует. Иначе вернуть ошибку.
 func (fe *Frontend) Put(k storage.RecordID, d []byte) error {
-	// TODO: implement
-	return nil
+	return fe.handleErrors(k, func(node storage.ServiceAddr) error {
+		return fe.cfg.NC.Put(node, k, d)
+	})
 }
 
 // Del an item from the storage if an item exists for the given key.
@@ -65,8 +96,9 @@ func (fe *Frontend) Put(k storage.RecordID, d []byte) error {
 // Del -- удалить запись из хранилища, если запись для данного ключа
 // существует. Иначе вернуть ошибку.
 func (fe *Frontend) Del(k storage.RecordID) error {
-	// TODO: implement
-	return nil
+	return fe.handleErrors(k, func(node storage.ServiceAddr) error {
+		return fe.cfg.NC.Del(node, k)
+	})
 }
 
 // Get an item from the storage if an item exists for the given key.
